46ec20c9708aae4f9e7712d4f7f3af7d5dab1afc
PICT-CyberCell <141261400+PICT-CyberCell@users.noreply.github.com>
2025-03-14 14:05:46 +0530

    confi files

---
+++ README.md
@@ -0,0 +1,3 @@
+# cybercelllllllllllllllll
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Kunj05/cybercelllllllllllllllll)
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      animation: {
+        'slow-spin': 'spin 20s linear infinite',
+      },
+      backgroundImage: {
+        'gradient-conic': 'conic-gradient(var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [],
+};
