3900600185afb79ceaf6e08b28b83ef08d816fa9
PICT-CyberCell <141261400+PICT-CyberCell@users.noreply.github.com>
2025-03-30 16:41:34 +0530

    Delete src/pages/api directory

--- src/pages/api/test.ts
+++
@@ -1,6 +0,0 @@
-import {  NextApiResponse } from 'next';
-
-export default function handler( res: NextApiResponse) {
-    res.status(200).json({ message: "API is working!" });
-  }
-//   vuqt ndck ekdl dxzh
